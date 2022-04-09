3da4340ac05891870cea15d11c907649aaf45a16
Justyna Samsel <01160265@pw.edu.pl>
2022-04-09 13:40:02 +0200

    end input

--- projekt.py
+++ projekt.py
@@ -5,8 +5,6 @@ Created on Tue Mar 29 12:27:21 2022
 @author: Justyna
 """
 
-# co jeszcze z wywolaniem odleglosci??
-# sprawdzic kolejnosc danych w def neu i azymut_elewacja ()
 
 
 import numpy as np
@@ -45,13 +43,15 @@ for ix in range(w):
     XY_92[ix] = elipsoida_grs80.u1992(fi_lam_h[ix,0], fi_lam_h[ix,1])
     n_e_u[ix] = elipsoida_grs80.neu(tablica[ix,0], tablica[ix,1], tablica[ix,2], tablica[ix,0]+1, tablica[ix,1]+1, tablica[ix,2]+1)
     tablica_wynikow[ix] = np.hstack([fi_lam_h[ix], XY_00[ix], XY_92[ix]])
-    
+# hstack --> automatyczne dopasowowywanie kolumn
+ 
 for ix in range(w-1):
     odleglosc_2D_3D[ix] = elipsoida_grs80.odleglosc_2D_3D(tablica[ix,0], tablica[ix,1], tablica[ix,2], tablica[ix+1,0], tablica[ix+1,1], tablica[ix+1,2])
-    azymut_elewacja[ix] = elipsoida_grs80.azymut_elewacja(tablica[ix,0], tablica[ix,1], tablica[ix,2], tablica[ix+1,0], tablica[ix+1,1], tablica[ix+1,2])
-    
-# hstack --> automatyczne dopasowowywanie kolumn
+    azymut_elewacja[ix] = elipsoida_grs80.azymut_elewacja(tablica[ix,0], tablica[ix,1], tablica[ix,2], tablica[ix+1,0], tablica[ix+1,1], tablica[ix+1,2])  
 
+
+
+### Panel dla uzytkownika
 
 print("Wybierz jedną z poniższych opcji:")
 while True:
@@ -72,36 +72,45 @@ while True:
             fi_lam_h[ix] = elipsoida_grs80.hirvonen(tablica[ix,0], tablica[ix,1], tablica[ix,2])
             print(f'{fi_lam_h[ix][0]} | {fi_lam_h[ix][1]} | {fi_lam_h[ix][2]}')
             
-    if  opcja == "2":
+    elif  opcja == "2":
         print('X | Y | Z')
         for ix in range(w):
             XYZ[ix] = elipsoida_grs80.geodezyjne2XYZ(fi_lam_h[ix,0], fi_lam_h[ix,1], fi_lam_h[ix,2])
             print(f'{ XYZ[ix][0] } |{ XYZ[ix][1] }|{ XYZ[ix][2] }')
     
-    if  opcja == "3":
+    elif  opcja == "3":
         print('N | E | U')
         for ix in range(w):
             n_e_u[ix] = elipsoida_grs80.neu(tablica[ix,0], tablica[ix,1], tablica[ix,2], tablica[ix,0]+1, tablica[ix,1]+1, tablica[ix,2]+1)
             print(f'{ n_e_u[ix][0] }|{ n_e_u[ix][1] }|{ n_e_u[ix][2] }')
             
             
-    if  opcja == "4":
-        print('X_układ2000 | Y_układ2000')
+    elif  opcja == "4":
+        print('X_układ_2000|Y_układ_2000')
         for ix in range(w):
             XY_00[ix] = elipsoida_grs80.u2000(fi_lam_h[ix,0], fi_lam_h[ix,1])
             print(f'{ XY_00[ix][0] }|{ XY_00[ix][1] }')
+    
+    elif  opcja == "5":
+        print('X_układ_1992|Y_układ_1992')
+        for ix in range(w):
+            XY_92[ix] = elipsoida_grs80.u1992(fi_lam_h[ix,0], fi_lam_h[ix,1])
+            print(f'{ XY_92[ix][0] }|{ XY_92[ix][1] }')
+            
+    elif  opcja == "6":
+        print('Azymut | Kąt elewacji')
+        for ix in range(w-1):
+            azymut_elewacja[ix] = elipsoida_grs80.azymut_elewacja(tablica[ix,0], tablica[ix,1], tablica[ix,2], tablica[ix+1,0], tablica[ix+1,1], tablica[ix+1,2])
+            print(f'{ azymut_elewacja[ix][0] }|{ azymut_elewacja[ix][1] }')
+    
+    elif  opcja == "7":
+        print('Odległość 2D | Odległość 3D')
+        for ix in range(w-1):
+            odleglosc_2D_3D[ix] = elipsoida_grs80.odleglosc_2D_3D(tablica[ix,0], tablica[ix,1], tablica[ix,2], tablica[ix+1,0], tablica[ix+1,1], tablica[ix+1,2])
+            print(f'{ odleglosc_2D_3D[ix][0] }|{ odleglosc_2D_3D[ix][1] }')
 
-
-
-
-
-#print('filamh',fi_lam_h)
-#print('00:',XY_00)
-#print('92:',XY_92)
-
-#print('odleglosc:',odleglosc_2D_3D)
-#print('neu:', n_e_u)            
-#print('Az_ev:', azymut_elewacja)                     
+    else:
+        print("\nWybrana opcja nie jest dostępna\n")              
 
 
 
