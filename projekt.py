e9785e7618b7456e6d21604200c6091ccc59e836
Justyna Samsel <01160265@pw.edu.pl>
2022-04-09 13:22:51 +0200

    add input

--- projekt.py
+++ projekt.py
@@ -26,6 +26,7 @@ w, k = np.shape(tablica)   # rozmiar tablicy (12,3)
 
 # puste talice do obliczen
 fi_lam_h = np.zeros((w, k))
+XYZ = np.zeros((w,k))
 XY_00 = np.zeros((w, 2))
 XY_92 = np.zeros((w, 2))
 n_e_u = np.zeros((w,k))
@@ -39,6 +40,7 @@ tablica_wynikow = np.zeros((w, 7))
 for ix in range(w):
     
     fi_lam_h[ix] = elipsoida_grs80.hirvonen(tablica[ix,0], tablica[ix,1], tablica[ix,2])
+    XYZ[ix] = elipsoida_grs80.geodezyjne2XYZ(fi_lam_h[ix,0], fi_lam_h[ix,1], fi_lam_h[ix,2])
     XY_00[ix] = elipsoida_grs80.u2000(fi_lam_h[ix,0], fi_lam_h[ix,1])
     XY_92[ix] = elipsoida_grs80.u1992(fi_lam_h[ix,0], fi_lam_h[ix,1])
     n_e_u[ix] = elipsoida_grs80.neu(tablica[ix,0], tablica[ix,1], tablica[ix,2], tablica[ix,0]+1, tablica[ix,1]+1, tablica[ix,2]+1)
@@ -51,16 +53,55 @@ for ix in range(w-1):
 # hstack --> automatyczne dopasowowywanie kolumn
 
 
+print("Wybierz jedną z poniższych opcji:")
+while True:
+    print("\nOpcje:")
+    print("1 | Transformacja współrzędnych geocentrycznych na współrzędne geodezyjne")
+    print("2 | Transformacja współrzędnych geodezyjnych na współrzędne geocentryczne")
+    print("3 | Obliczenie współrzędnych topocentrycznych E,N,U")
+    print("4 | Transformacja współrzędnych geodezyjnych do układu płaskiego 2000")
+    print("5 | Transformacja współrzędnych geodezyjnych do układu płaskiego 1992")
+    print("6 | Wyznaczenie kąta azymutu i kąta elewacji")
+    print("7 | Wyznaczenie odległości 2D oraz 3D")
+
+    opcja = input()
+
+    if  opcja == "1":
+        print('Szerokosc geodezyjna | Dlugosc geodezyjna | Wysokosc na elipsoidzie')
+        for ix in range(w):
+            fi_lam_h[ix] = elipsoida_grs80.hirvonen(tablica[ix,0], tablica[ix,1], tablica[ix,2])
+            print(f'{fi_lam_h[ix][0]} | {fi_lam_h[ix][1]} | {fi_lam_h[ix][2]}')
+            
+    if  opcja == "2":
+        print('X | Y | Z')
+        for ix in range(w):
+            XYZ[ix] = elipsoida_grs80.geodezyjne2XYZ(fi_lam_h[ix,0], fi_lam_h[ix,1], fi_lam_h[ix,2])
+            print(f'{ XYZ[ix][0] } |{ XYZ[ix][1] }|{ XYZ[ix][2] }')
+    
+    if  opcja == "3":
+        print('N | E | U')
+        for ix in range(w):
+            n_e_u[ix] = elipsoida_grs80.neu(tablica[ix,0], tablica[ix,1], tablica[ix,2], tablica[ix,0]+1, tablica[ix,1]+1, tablica[ix,2]+1)
+            print(f'{ n_e_u[ix][0] }|{ n_e_u[ix][1] }|{ n_e_u[ix][2] }')
+            
+            
+    if  opcja == "4":
+        print('X_układ2000 | Y_układ2000')
+        for ix in range(w):
+            XY_00[ix] = elipsoida_grs80.u2000(fi_lam_h[ix,0], fi_lam_h[ix,1])
+            print(f'{ XY_00[ix][0] }|{ XY_00[ix][1] }')
 
 
-# Zrobić tu inputa
-print('filamh',fi_lam_h)
-print('00:',XY_00)
-print('92:',XY_92)
 
-print('odleglosc:',odleglosc_2D_3D)
-print('neu:', n_e_u)            
-print('Az_ev:', azymut_elewacja)                     
+
+
+#print('filamh',fi_lam_h)
+#print('00:',XY_00)
+#print('92:',XY_92)
+
+#print('odleglosc:',odleglosc_2D_3D)
+#print('neu:', n_e_u)            
+#print('Az_ev:', azymut_elewacja)                     
 
 
 
